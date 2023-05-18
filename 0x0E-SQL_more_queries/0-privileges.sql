cf6749268f2f56d91a771bdf1868edce481be6d5
Nurudeen Oyelowo <neroevali4u@yahoo.co.uk>
2023-05-18 08:55:12 -0700

    SQL 2

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Lists all privileges of Users `user_0d_1`
+-- and `user_0d_2` in the localhost.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- Creating the table `force_name`
+-- in the MySQL Server.
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
